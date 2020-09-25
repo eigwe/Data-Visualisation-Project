7da9de7c92fd2aeb27fe0c0ee2c019e0e9ffc5f6
Emmanuel Igwe <emmanuel_igwe@ymail.com>
2020-09-25 15:49:00 +0100

    This Data Visualisation shows an example of using data for analysis

---
+++ Data Visualisation Script.R
@@ -0,0 +1,32 @@
+#Data Visualisation Project
+
+library(ggplot2)
+library(ggthemes)
+library(data.table)
+
+pointsToLabel <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
+                   "Afghanistan", "Congo", "Greece", "Argentina", "Brazil",
+                   "India", "Italy", "China", "South Africa", "Spane",
+                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
+                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
+                   "New Zealand", "Singapore")
+
+df <- read.csv("C:\\Users\\emman\\OneDrive\\Documents\\R\\R-Course-HTML-Notes\\R-for-Data-Science-and-Machine-Learning\\Training Exercises\\Capstone and Data Viz Projects\\Data Visualization Project\\Economist_Assignment_Data.csv", header = TRUE)
+df
+head(df)
+str(df)
+
+#Scatterplot
+
+pl <- ggplot(data = df,aes(x=CPI, y=HDI, color = Region)) + geom_point(size = 4,shape=1)
+
+pl2 <- pl + geom_smooth(aes(group=1),metho='lm',formula = y~log(x),se=F,COLOR='red')
+
+pl3 <- pl2 + geom_text(aes(label = Country), color = "gray20", 
+                       data = subset(df, Country %in% pointsToLabel),check_overlap = TRUE)
+
+pl4 <- pl3 + scale_x_continuous(limits=c(.9,10.5),breaks=1:10)
+
+print(pl4 + theme_economist_white())
+
+
